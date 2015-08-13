24529004b8cb8abd3eba987e73978a5afb0c049c
Jouni Kallunki <jouni.kallunki@reaktor.fi>
2015-08-13 08:44:07 +0300

    plot all axes to single pdf

--- history_plots.py
+++ history_plots.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import urllib2
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
+from matplotlib.backends.backend_pdf import PdfPages
 
 www = 'http://flow.rista.net/logs'
 
@@ -32,6 +33,68 @@ def read_files():
             except:
                 pass
     return msgs
+
+
+def plot_axis(spec, idx, s, title, ylabel=None, bgimg=None):
+    ax = plt.subplot(spec)
+    ax.set_title(title)
+    ax.plot_date(idx.to_pydatetime(), s, '-')
+    ax.xaxis.set_major_locator(dates.HourLocator(byhour=(0, 4, 8, 12, 16, 20)))
+    ax.xaxis.set_major_formatter(dates.DateFormatter('%H'))
+    ax.xaxis.set_minor_locator(dates.DayLocator())
+    ax.xaxis.set_minor_formatter(dates.DateFormatter('\n%d-%m-%Y'))
+    if bgimg is not None:
+        x0, x1 = ax.get_xlim()
+        y0, y1 = ax.get_ylim()
+        ax.imshow(bgimg, extent=[x0, x1, y0, y1], aspect='auto')
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    plt.show()
+    return ax
+
+
+def plot_all_to_pdf(dfs, sensors, bgimg, fname):
+
+    fig = plt.figure(figsize=(23, 12))
+    plt.subplots_adjust(left=0.05, right=0.95)
+    i = 0
+    idx = dfs[i].index
+    s = dfs[i]['Illuminance log10']
+    plot_axis(323, idx, s, 'Illuminance',
+              ylabel='lx', bgimg=bgimg)
+
+    i = 1
+    idx = dfs[i].index
+    s = (4095.0 - dfs[i]['Rain drops']) / 4095.0 * 100.0
+    cond = (s >= 0)
+    s = s[cond]
+    idx = idx[cond]
+    plot_axis(321, idx, s, 'Rain drops', bgimg=bgimg)
+
+    idx = dfs[i].index
+    s = (4095.0 - dfs[i]['Soil moisture']) / 4095.0 * 100.0
+    cond = (s >= 0)
+    s = s[cond]
+    idx = idx[cond]
+    plot_axis(322, idx, s, 'Soil moisture', bgimg=bgimg)
+
+    # i = 2
+    # idx = dfs[i].index
+    # s = dfs[i].icol(0)
+    # plot_axis(324, idx, s, '{}\nTemperature'.format(sensors[i]), 'Celsius', bgimg=bgimg)
+
+    i = 3
+    idx = dfs[i].index
+    s = dfs[i]['Air pressure']
+    plot_axis(325, idx, s, 'Air pressure', 'mbar', bgimg=bgimg)
+
+    s = dfs[i]['Temperature']
+    plot_axis(326, idx, s, 'Temperature', 'Celsius', bgimg=bgimg)
+
+    with PdfPages(fname) as pdf:
+        pdf.savefig()
+        plt.close()
+    return fig
 
 
 def plot_data(idx, s, title, ylabel=None, fname=None, y_log_scale=False, bgimg=None):
@@ -117,8 +180,9 @@ def main():
         dfs.append(grpd.get_group(k).dropna(axis=1, how='all'))
         sensors.append(k)
     bgimg = plt.imread('flowtalksbg.png')
-    onedf = resample(df)
-    plot_all_to_files(dfs, sensors, bgimg)
+    # onedf = resample(df)
+    plot_all_to_pdf(dfs, sensors, bgimg, 'plots/parrulaituri.pdf')
+    # plot_all_to_files(dfs, sensors, bgimg)
 
 
 if __name__ == '__main__':
